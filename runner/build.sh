7cfa60a78071d4a2e00433744798f692fd11113b
Laurence Tratt <laurie@tratt.net>
2018-04-14 00:11:36 +0100

    We're not going to run cpctplusdyndist for this version of the paper.

--- runner/build.sh
+++ runner/build.sh
@@ -31,6 +31,5 @@ if [ ! -d grammars ]; then
 fi
 
 ./run.py src_files lrpar/target/release/lrpar cpctplus grammars/java7/java.l grammars/java7/java.y cpctplus.csv
-./run.py src_files lrpar/target/release/lrpar cpctplusdyndist grammars/java7/java.l grammars/java7/java.y cpctplusdyndist.csv
 ./run.py src_files lrpar/target/release/lrpar mf grammars/java7/java.l grammars/java7/java.y mf.csv
 ./run.py src_files lrpar_rev/target/release/lrpar mf grammars/java7/java.l grammars/java7/java.y mf_rev.csv
