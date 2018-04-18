2e1811a5b2bfe476c051e9ed95882ea24576de02
Laurence Tratt <laurie@tratt.net>
2018-04-18 16:37:32 +0100

    Separate building the experiments from running the experiments.

--- runner/build.sh
+++ runner/build.sh
@@ -35,7 +35,3 @@ if [ ! -d grammars ]; then
     cd grammars && git checkout ${GRAMMARSV}
     cd ..
 fi
-
-./run.py src_files lrpar/target/release/lrpar cpctplus grammars/java7/java.l grammars/java7/java.y cpctplus.csv
-./run.py src_files lrpar/target/release/lrpar mf grammars/java7/java.l grammars/java7/java.y mf.csv
-./run.py src_files lrpar_rev/target/release/lrpar mf grammars/java7/java.l grammars/java7/java.y mf_rev.csv
